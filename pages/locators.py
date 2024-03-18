f7eef4c8c83dfedce9981dd5c95b2af5f82e1c93
Михальченко Дмитрий <140504256+MikhalchenkoD@users.noreply.github.com>
2024-03-18 16:37:07 +0500

    add ItemPage and new test

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ItemPageLocators:
+    CART_BTN = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    TITLE = (By.CSS_SELECTOR, '.product_main h1')
+    PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    TITLE_IN_MESSAGE = (By.CSS_SELECTOR, '#messages .alertinner strong')
+    PRICE_IN_MESSAGE = (By.CSS_SELECTOR, '#messages .alertinner p strong')
